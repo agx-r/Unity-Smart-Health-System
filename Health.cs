55133325a146bc64586962c09b86306a68101c4b
AGX <55353943+agx-r@users.noreply.github.com>
2023-07-30 15:00:07 +0500

    Update Health.cs

--- Health.cs
+++ Health.cs
@@ -9,6 +9,12 @@ public class Health : MonoBehaviour
     [SerializeField] private int maxHealth = 100; // Maximum health value for the object
     private int currentHealth; // Current health value for the object
 
+    // Public property for accessing the current health value
+    public int CurrentHealth => currentHealth;
+
+    // Public property for accessing the maximum health value
+    public int MaxHealth => maxHealth;
+
     private void Start()
     {
         currentHealth = maxHealth; // Initialize current health to maxHealth when the object starts
@@ -17,6 +23,12 @@ public class Health : MonoBehaviour
     // Function to take damage
     public void TakeDamage(int damageAmount)
     {
+        if (damageAmount < 0)
+        {
+            Debug.LogWarning("Damage amount should be a positive value. Use Heal method to heal.");
+            return;
+        }
+
         currentHealth -= damageAmount; // Reduce current health by the damage amount
 
         // Check if health has dropped below 0 (dead)
@@ -29,6 +41,12 @@ public class Health : MonoBehaviour
     // Function to heal
     public void Heal(int healAmount)
     {
+        if (healAmount < 0)
+        {
+            Debug.LogWarning("Heal amount should be a positive value. Use TakeDamage method to take damage.");
+            return;
+        }
+
         currentHealth += healAmount; // Increase current health by the heal amount
 
         // Make sure currentHealth doesn't exceed maxHealth
