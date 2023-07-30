ea38294bb0e46fe723b05fe79cb2c1e5e36f9943
Floataloon <138375427+LunariaStardrop@users.noreply.github.com>
2023-07-30 16:07:11 +0500

    Refactored AGX's shit

--- HealthDisplay.cs
+++ HealthDisplay.cs
@@ -3,35 +3,31 @@ using UnityEngine.UI;
 
 public class HealthDisplay : MonoBehaviour
 {
-    [SerializeField]
-    private TextMesh healthTextMesh; // Reference to the TextMesh component that will display health
-
-    [SerializeField]
-    private Slider healthSlider; // Reference to the Slider component that will display health
+    [SerializeField] private TextMesh healthTextMesh; // Reference to the TextMesh component that will display health
+    [SerializeField] private Slider healthSlider; // Reference to the Slider component that will display health
 
     private Health healthComponent; // Reference to the Health component
 
     private void Awake()
     {
         healthComponent = GetComponent<Health>(); // Get the Health component attached to the same GameObject
+        UpdateHealthDisplay(healthComponent.CurrentHealth, healthComponent.MaxHealth);
     }
 
     private void OnEnable()
     {
-        // Subscribe to the OnHealthChange event of the Health component
-        Health.OnHealthChange += UpdateHealthDisplay;
+        healthComponent.OnHealthChange += UpdateHealthDisplay; // Subscribe to the OnHealthChange event of the Health component
     }
 
     private void OnDisable()
     {
-        // Unsubscribe from the OnHealthChange event when this script is disabled or destroyed
-        Health.OnHealthChange -= UpdateHealthDisplay;
+        healthComponent.OnHealthChange -= UpdateHealthDisplay; // Unsubscribe from the OnHealthChange event when this script is disabled or destroyed
     }
 
     private void UpdateHealthDisplay(int currentHealth, int maxHealth)
     {
         // Update the TextMesh text with the current health value
-        healthTextMesh.text = "Health: " + currentHealth.ToString();
+        healthTextMesh.text = "Health: " + currentHealth;
 
         // Update the Slider value based on the current health
         healthSlider.value = (float)currentHealth / maxHealth;
